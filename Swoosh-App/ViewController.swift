21a3f9191e0e9da33fe23f4824c6056b1274fca8
Mirshad Oz <mirshadoz@mirshads-macbook-pro.local>
2018-08-28 20:01:49 +0600

    Initial Commit

---
+++ Swoosh-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swoosh-App
+//
+//  Created by Mirshad Oz on 8/28/18.
+//  Copyright © 2018 Mirshad Oz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
